6dd589d25c73489ed7637311f5461bc1984e66cc
Daniel Coelho <danielmcoelho.br@gmail.com>
2026-04-14 15:36:43 -0300

    chore: repository cleanup for production v2 deployment

--- dump_check.ts
+++
@@ -1,6 +0,0 @@
-import { questionnairesData } from './src/data/questionnaires';
-import fs from 'fs';
-
-const dump = JSON.stringify(questionnairesData.afCervical.sections.find(s => s.id === 'exame_neurologico'), null, 2);
-fs.writeFileSync('dump.json', dump);
-console.log('Dumped');
--- test-fm.js
+++
@@ -1,7 +0,0 @@
-try {
-    const fm = require('framer-motion');
-    console.log('framer-motion found:', typeof fm);
-} catch (e) {
-    console.error('framer-motion NOT found');
-    process.exit(1);
-}
